8aaee737519f83c00950bfc5b8294ac79e7b19b1
Martin M. S. Pedersen <traxplayer@gmail.com>
2019-01-03 13:43:42 +0100

    x

--- bin/check-a-vs-b.R
+++ bin/check-a-vs-b.R
@@ -3,10 +3,10 @@ library(tidyverse)
 library(magrittr)
 library(reshape2)
 
-# synthesized <- function(f) {
-#   grepl("(b-file synthesized from sequence entry)",readLines(f,1),fixed = TRUE)
-# }
-#
+synthesized <- function(f) {
+  grepl("(b-file synthesized from sequence entry)",readLines(f,1),fixed = TRUE)
+}
+
 # A <- read.table("~/oeis/afiles/stripped", quote="\"", stringsAsFactors=FALSE)
 # names(A) <- c("id","avalues")
 #
@@ -16,7 +16,7 @@ library(reshape2)
 #   glimpse
 #
 # save(A,file = "anumbers.Rda")
-#
+
 # load("anumbers.Rda")
 #
 setwd("~/oeis/bfiles/")
@@ -25,6 +25,7 @@ setwd("~/oeis/bfiles/")
 #                 bvalues = character(0),
 #                 synthesized = logical(0),
 #                 stringsAsFactors = FALSE)
+#
 # for (bfile in list.files(pattern = "b[0-9]{6}\\.txt")) {
 #   bnum <- substring(bfile,2,7)
 #   if (synthesized(bfile)) {
@@ -37,24 +38,60 @@ setwd("~/oeis/bfiles/")
 # }
 #
 # B$id <- as.character(B$id)
-# save(B,file = "bnumbers.Rda")
+# #save(B,file = "bnumbers.Rda")
 # C <- left_join(A,B,"id")
 #save(A,B,C,file = "numbers.Rda")
 
 getBvalues <- function(id, howmany) {
+#  setwd("~/oeis/bfiles/")
+  re <- "^ *#|^ *$"
   fname <- paste0("b",id,".txt")
+  #if (howmany < 25) message(fname)
   message(fname)
-  paste0(",",paste0(sub(".* ","",readLines(fname,howmany)),",", collapse=""))
+  extra <- 0
+  while (sum(!grepl(re,readLines(fname,howmany+extra))) < howmany) {
+    extra <- extra + 1
+    if (extra > 100) {
+      message("Strange: ",fname)
+      return(NA)
+    }
+  }
+  lines <- readLines(fname,howmany + extra)
+  lines <- lines[!grepl(re,lines)]
+  lines <- sub(" +$","",lines)
+  lines <- sub("\\t"," ",lines)
+  lines <- sub(" *#.*$","",lines)
+  paste0(",",paste0(sub(".* ","",lines),",", collapse=""))
 }
 
 #load("numbers.Rda")
 #save(C, file = "cnumbers.Rda")
-load("cnumbers.Rda")
+#load("cnumbers.Rda")
 for (aid in C[C$synthesized == FALSE & is.na(C$bvalues),]$id) {
 #for (aid in head(C[C$synthesized == FALSE & is.na(C$bvalues),]$id,50000)) {
   b <- getBvalues(aid,C[C$id == aid,]$acount)
   #print(b)
-  C[which(C$id == aid),]$bvalues <- b
-
+  if (!is.na(b)) {
+    C[which(C$id == aid),]$bvalues <- b
+  }
 }
 save(C,file = "cnumbers.Rda")
+
+#load("cnumbers.Rda")
+
+sum(C$avalues != C$bvalues & C$synthesized == FALSE & !is.na(C$bvalues))
+head(C[C$avalues != C$bvalues & C$synthesized == FALSE & !is.na(C$bvalues),],1)
+
+C %>%
+  filter(avalues != bvalues & synthesized == FALSE & !is.na(C$bvalues)) %>%
+  filter(id != "002104") %>%
+  filter(id != "002828") %>%
+  filter(id != "007099") %>%
+  filter(id != "007504") %>%
+  filter(id != "013235") %>%
+  filter(id != "013603") %>%
+  filter(id != "015858") %>%
+  filter(id != "023847") %>%
+  head(1)
+#C[(C$avalues != C$bvalues) & !is.na(C$bvalues),]$bvalues <- NA
+
